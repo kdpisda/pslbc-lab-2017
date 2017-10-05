a43f46a8e8cc6be7fc740dbda5675ab1f3a9abb1
Kuldeep Pisda <pisdak79@gmail.com>
2017-10-05 19:39:57 +0530

    Added 14th pattern

--- 1st_assignment/16115036_clab_14.c
+++ 1st_assignment/16115036_clab_14.c
@@ -6,7 +6,15 @@ int main(){
     for(i = n; i > 0; i--){
         for(k = 0; k < i; k++)
             printf(" ");
-        for(j = 0; j < n-i; j++){
+        for(j = 0; j <= 2*(n-i); j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i = 1; i < n; i++){
+        for(k = -1; k < i; k++)
+            printf(" ");
+        for(j = 0; j < 2*(n-i)-1; j++){
             printf("*");
         }
         printf("\n");
